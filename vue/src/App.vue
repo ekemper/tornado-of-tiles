9ca212ea8607084156391fffb9a823d8aa5ff5f4
Edward Kemper <edwardkemper@gmail.com>
2018-05-12 11:47:41 -0600

    styling

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -11,7 +11,22 @@ export default {
 </script>
 
 <style>
+
+html {
+  height: 100%;
+}
+
+body {
+  margin:0;
+  padding:0;
+  width:100%;
+  height: 100%;
+  min-height: 100%;
+}
+
 #app {
+  background-color: #424242;
+  height: 100%;
 }
 
 </style>
--- vue/src/components/HelloWorld.vue
+++ vue/src/components/HelloWorld.vue
@@ -1,51 +1,55 @@
 <template>
-<div>
-
   <div class="container">
-    <div class="row">
-      <div class="col-md-10 messages">
-        <div v-for="message in messages" :key="message.id">
-          <span>
-            {{message.user}}
-          </span>
-          <span>
-            {{message.body}}
-          </span>
-        </div>
+    <!-- <div class="row">
+      <div class="col-md-10"> -->
 
-      </div>
-    </div>
+        <div class="card">
+          <div class="card-body messages">
 
-    <div class="row message-input">
-      <div class="col-sm">
+            <ul class="list-group">
+              <li
+                class="list-group-item"
+                v-for="message in messages" :key="message.id"
+              >
+                <span class="user">
+                  {{message.user}}:
+                </span>
+                <span>
+                  {{message.body}}
+                </span>
+              </li>
+            </ul>
 
-        <div class="input-group mb-3">
-          <input
-            type="text"
-            class="form-control"
-            placeholder=""
-            aria-label="message input"
-            v-on:keyup.13="submit"
-            v-model="newMessage"
-          >
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button">
-              <i class="fas fa-angle-right"></i>
-            </button>
+          </div>
+          <div class="card-footer bg-transparent">
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control"
+                placeholder=""
+                aria-label="message input"
+                v-on:keyup.13="submit"
+                v-model="newMessage"
+              >
+              <div class="input-group-append">
+                <button class="btn" type="button">
+                  <i class="fas fa-angle-right"></i>
+                </button>
+              </div>
+            </div>
+
           </div>
         </div>
 
-      </div>
-    </div>
+      <!-- </div>
+    </div> -->
   </div>
-
-</div>
 </template>
 
 <script>
 
 export default {
-  name: 'main',
+  name: 'messages',
   data () {
     return {
       messages: [
@@ -81,7 +85,48 @@ function submit () {
 </script>
 
 <style>
-  .message-input {
-    bottom: 0px;
+
+  .container {
+    height: 100%;
   }
+
+  .card {
+    height: 100%;
+    background-color: #333;
+  }
+
+  .messages {
+    height: 85%;
+  }
+
+  .message-input {
+    background-color: #424242;
+    bottom: 0px;
+    margin-top: 10px;
+
+  }
+
+  .card-footer .input-group input.form-control {
+    background-color: #424242;
+    color: #aaa;
+  }
+
+  .user {
+    font-weight: bold;
+  }
+
+  li.list-group-item {
+    background-color: #424242;
+    border: none;
+    color: #aaa;
+  }
+
+  .btn {
+    background-color: #bbb;
+  }
+
+  .user {
+    color: #000;
+  }
+
 </style>
